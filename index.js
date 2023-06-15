7ed8bf86d48af6586b3eb3ef141b11d4c1ed24d3
Minh Duc Huynh <n01583853@humber.ca>
2023-06-15 01:17:03 -0400

    Edit codeBase

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-// Import essential libraries 
-const express = require('express'); 
-const app = express(); 
-const path = require('path'); 
+// Import essential libraries
+const express = require("express");
+const app = express();
+const path = require("path");
+// Enject pug
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+// set Directory in public Folder
 app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) =>{
-    res.render("index", { title: "Home" });
-})
-app.get("/About" , (req, res) => {
-    res.render("about"), { title: "About"};
-})
-app.get("/yujia", (req, res)=>{
-    res.render("yujia", {title : "Yujia"} );
-})
-app.listen(process.env.port || 3000); 
-console.log('Running at Port 3000'); 
+// Add Json convert or sanitize
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// set up .env
+require("dotenv").config();
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.listen(process.env.port || 8080);
+console.log("Running at Port 3000");
